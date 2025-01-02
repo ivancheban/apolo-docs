79ec46d9566a4b440b031901670c20fb1cb2499c
Ivan Cheban <ivancheban@gmail.com>
2025-01-02 18:28:00 +0200

    first commit

---
+++ docs/cli-reference/README.md
@@ -0,0 +1,23 @@
+---
+sidebar_position: 1
+---
+
+# Introduction
+
+Installation process is decribed in [Getting Started](../../getting-started.md#installing-cli) guide.
+
+## Sections
+
+* [Shortcuts](./commands/shortcuts.md): common operations
+* [neuro job](./commands/job.md): jobs operations
+* [neuro storage](./commands/storage.md): managing storage
+* [neuro image](./commands/image.md): Docker images (environments) manipulations
+* [neuro project](./project.md): project operations
+* [neuro config](./commands/config.md): client configuration
+* [neuro secret](./commands/secret.md): operations with secrets
+* [neuro acl](./commands/acl.md): permissions and access management
+* [neuro blob](blob.md): blob storage operations
+* [neuro admin](./commands/admin.md): cluster administration
+* [neuro completion](./commands/completion.md): shell completion code generation
+* [Topics](./topics.md): additional information (e.g. user config location and format)
+
---
+++ docusaurus.config.js
@@ -0,0 +1,137 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import { themes as prismThemes } from "prism-react-renderer";
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Apolo Documentation",
+  tagline: "Dinosaurs are cool",
+  favicon: "img/apolo.ico",
+
+  // Set the production url of your site here
+  url: "https://your-docusaurus-site.example.com",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "ivancheban", // Usually your GitHub org/user name.
+  projectName: "apolo-docs", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: "/",
+          sidebarPath: "./sidebars.js",
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: "https://github.com/ivancheban/apolo-docs/tree/main/",
+        },
+        blog: false,
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: "img/docusaurus-social-card.jpg",
+      navbar: {
+        title: "Apolo Documentation",
+        logo: {
+          alt: "Apolo Logo",
+          src: "img/apolo.png",
+        },
+        items: [
+          {
+            type: "docSidebar",
+            sidebarId: "conceptsSidebar",
+            position: "left",
+            label: "Concepts",
+          },
+          {
+            type: "docSidebar",
+            sidebarId: "cliSidebar",
+            position: "left",
+            label: "CLI Reference",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Docs",
+            items: [
+              {
+                label: "Tutorial",
+                to: "/docs/intro",
+              },
+            ],
+          },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Stack Overflow",
+                href: "https://stackoverflow.com/questions/tagged/docusaurus",
+              },
+              {
+                label: "Discord",
+                href: "https://discordapp.com/invite/docusaurus",
+              },
+              {
+                label: "X",
+                href: "https://x.com/docusaurus",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "Blog",
+                to: "/blog",
+              },
+              {
+                label: "GitHub",
+                href: "https://github.com/facebook/docusaurus",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} All rights reserved. Ivan Cheban`,
+      },
+      prism: {
+        theme: prismThemes.oneDark,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
